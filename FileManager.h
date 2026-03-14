0cc229d502c2848330844425d3613d317de2cf79
Шаисламов Михаил <107649292+Fohin6@users.noreply.github.com>
2026-03-14 17:43:17 +0500

    AES-256 crypt

--- FileManager.h
+++ FileManager.h
@@ -3,68 +3,188 @@
 
 #include <QObject>
 #include <QFile>
-#include <QTextStream>
-#include <QCryptographicHash>
 #include <QDebug>
-#include <QCoreApplication> // Добавлено!
+#include <QCoreApplication>
+#include <QCryptographicHash>
+
+#include <openssl/evp.h>
+#include <openssl/rand.h>
 
 class FileManager : public QObject
 {
-    Q_OBJECT // Добавлено! Это обязательно для Q_INVOKABLE
+    Q_OBJECT
 
 public:
     explicit FileManager(QObject* parent = nullptr) : QObject(parent) {}
 
-    Q_INVOKABLE bool saveMasterPassword(const QString& password) {
-        // Создаем хеш пароля
-        QByteArray passwordHash = QCryptographicHash::hash(
-            password.toUtf8(),
-            QCryptographicHash::Sha256
-        ).toHex();
+private:
 
-        // Путь к файлу в папке приложения
-        QString filePath = QCoreApplication::applicationDirPath() + "/master.hash";
-        qDebug() << "Saving to:" << filePath; // Отладка
+    QByteArray encryptAES256GCM(const QByteArray& plaintext,
+        const QByteArray& key,
+        QByteArray& iv,
+        QByteArray& tag)
+    {
+        EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
 
-        QFile file(filePath);
-        if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
-            qDebug() << "Cannot open file for writing:" << file.errorString();
-            return false;
+        iv.resize(12);
+        RAND_bytes(reinterpret_cast<unsigned char*>(iv.data()), iv.size());
+
+        EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), nullptr, nullptr, nullptr);
+
+        EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, iv.size(), nullptr);
+
+        EVP_EncryptInit_ex(ctx, nullptr, nullptr,
+            reinterpret_cast<const unsigned char*>(key.data()),
+            reinterpret_cast<const unsigned char*>(iv.data()));
+
+        QByteArray ciphertext;
+        ciphertext.resize(plaintext.size());
+
+        int len;
+        EVP_EncryptUpdate(ctx,
+            reinterpret_cast<unsigned char*>(ciphertext.data()),
+            &len,
+            reinterpret_cast<const unsigned char*>(plaintext.data()),
+            plaintext.size());
+
+        int ciphertext_len = len;
+
+        EVP_EncryptFinal_ex(ctx,
+            reinterpret_cast<unsigned char*>(ciphertext.data()) + len,
+            &len);
+
+        ciphertext_len += len;
+
+        ciphertext.resize(ciphertext_len);
+
+        tag.resize(16);
+        EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, 16, tag.data());
+
+        EVP_CIPHER_CTX_free(ctx);
+
+        return ciphertext;
+    }
+
+
+    QByteArray decryptAES256GCM(const QByteArray& ciphertext,
+        const QByteArray& key,
+        const QByteArray& iv,
+        const QByteArray& tag)
+    {
+        EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
+
+        EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), nullptr, nullptr, nullptr);
+
+        EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, iv.size(), nullptr);
+
+        EVP_DecryptInit_ex(ctx, nullptr, nullptr,
+            reinterpret_cast<const unsigned char*>(key.data()),
+            reinterpret_cast<const unsigned char*>(iv.data()));
+
+        QByteArray plaintext;
+        plaintext.resize(ciphertext.size());
+
+        int len;
+        EVP_DecryptUpdate(ctx,
+            reinterpret_cast<unsigned char*>(plaintext.data()),
+            &len,
+            reinterpret_cast<const unsigned char*>(ciphertext.data()),
+            ciphertext.size());
+
+        int plaintext_len = len;
+
+        EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, tag.size(),
+            const_cast<char*>(tag.data()));
+
+        int ret = EVP_DecryptFinal_ex(ctx,
+            reinterpret_cast<unsigned char*>(plaintext.data()) + len,
+            &len);
+
+        EVP_CIPHER_CTX_free(ctx);
+
+        if (ret <= 0)
+        {
+            qDebug() << "Decryption failed (authentication error)";
+            return {};
         }
 
-        QTextStream out(&file);
-        out << passwordHash;
+        plaintext_len += len;
+        plaintext.resize(plaintext_len);
+
+        return plaintext;
+    }
+
+
+public:
+
+    Q_INVOKABLE bool saveMasterPassword(const QString& password)
+    {
+        QString filePath = QCoreApplication::applicationDirPath() + "/master.dat";
+
+        QFile file(filePath);
+        if (!file.open(QIODevice::WriteOnly))
+            return false;
+
+        QByteArray key = QCryptographicHash::hash(
+            "super_secret_key",
+            QCryptographicHash::Sha256
+        );
+
+        QByteArray iv;
+        QByteArray tag;
+
+        QByteArray encrypted = encryptAES256GCM(
+            password.toUtf8(),
+            key,
+            iv,
+            tag
+        );
+
+        file.write(iv);
+        file.write(tag);
+        file.write(encrypted);
+
         file.close();
 
-        qDebug() << "Master password hash saved to:" << filePath;
         return true;
     }
 
-    Q_INVOKABLE bool verifyMasterPassword(const QString& password) {
-        QString filePath = QCoreApplication::applicationDirPath() + "/master.hash";
+
+    Q_INVOKABLE bool verifyMasterPassword(const QString& password)
+    {
+        QString filePath = QCoreApplication::applicationDirPath() + "/master.dat";
+
         QFile file(filePath);
-
-        if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-            qDebug() << "Cannot open file for reading:" << file.errorString();
+        if (!file.open(QIODevice::ReadOnly))
             return false;
-        }
 
-        QTextStream in(&file);
-        QString savedHash = in.readAll();
+        QByteArray iv = file.read(12);
+        QByteArray tag = file.read(16);
+        QByteArray ciphertext = file.readAll();
+
         file.close();
 
-        QByteArray inputHash = QCryptographicHash::hash(
-            password.toUtf8(),
+        QByteArray key = QCryptographicHash::hash(
+            "super_secret_key",
             QCryptographicHash::Sha256
-        ).toHex();
+        );
 
-        return savedHash == inputHash;
+        QByteArray decrypted = decryptAES256GCM(
+            ciphertext,
+            key,
+            iv,
+            tag
+        );
+
+        return decrypted == password.toUtf8();
     }
 
-    Q_INVOKABLE bool isMasterPasswordSet() {
-        QString filePath = QCoreApplication::applicationDirPath() + "/master.hash";
+
+    Q_INVOKABLE bool isMasterPasswordSet()
+    {
+        QString filePath = QCoreApplication::applicationDirPath() + "/master.dat";
         return QFile::exists(filePath);
     }
 };
 
-#endif // FILEMANAGER_H
+#endif
